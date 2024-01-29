379c83c7c22ae7f642177b823a05ddc57b1c71b4
@sergesiluyanov <siluyanov@udev.dev>
2024-01-29 14:36:20 +0400

    add few more tests

---
+++ cypress/e2e/api_tests/workout/post_new_workout.cy.js
@@ -0,0 +1,31 @@
+const { expect } = require("chai");
+const { request } = require("http");
+
+let tokenvalue
+
+describe('hiq api test', () => {
+beforeEach(() => {
+  cy.fixture('user_data').then(loginFixture => {
+    cy.request({
+      method: 'POST',
+      url: 'http://54.193.113.143:4000/auth/signin',
+      body: loginFixture
+    }).then(response => {
+      tokenvalue = response.body.data['accessToken'];
+    });
+})
+})
+
+it('get workouts data', () => {
+      cy.request({
+          headers: {
+          Authorization: 'Bearer '+tokenvalue,
+          accept: 'application/json'
+        },
+        method: 'POST',
+        url: 'http://54.193.113.143:4000/workouts'
+      }).then(response => {
+        expect(response.status).to.eq(201)
+      }); 
+})
+})
